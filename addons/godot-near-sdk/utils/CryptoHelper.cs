40bae7d5bd0ce94a7d60a209d298d05590194e72
svntax <endless.answers@gmail.com>
2021-08-15 23:43:10 -0400

    Added web-compatible base58 encoding library for keypair creation.

--- addons/godot-near-sdk/utils/CryptoHelper.cs
+++ addons/godot-near-sdk/utils/CryptoHelper.cs
@@ -1,33 +1,28 @@
 using Godot;
 using System;
 using Rebex.Security.Cryptography;
+using SimpleBase;
 
 public class CryptoHelper : Node {
 
-    public byte[] publicKey;
-    public byte[] privateKey;
+    public string publicKey;
+    public string privateKey;
 
     public override void _Ready(){
-        GD.Print("Hello from C#");
+        // Empty
     }
 
     public void CreateKeyPair(){
         byte[][] keypair = new byte[2][];
 
         var ed = new Ed25519();
-        this.publicKey = ed.GetPublicKey();
-        this.privateKey = ed.GetPrivateKey();
+        byte[] publicKeyBytes = ed.GetPublicKey();
+        this.publicKey = Base58.Bitcoin.Encode(publicKeyBytes);
+        byte[] privateKeyBytes = ed.GetPrivateKey();
+        this.privateKey = Base58.Bitcoin.Encode(privateKeyBytes);
 
-        String output = "";
-        for(int i = 0; i < publicKey.Length; i++){
-            output += publicKey[i] + ",";
-        }
-        GD.Print("Public key: " + output);
-        output = "";
-        for(int i = 0; i < privateKey.Length; i++){
-            output += privateKey[i] + ",";
-        }
-        GD.Print("Private key: " + output);
+        GD.Print("Public key: " + this.publicKey);
+        GD.Print("Private key: " + this.privateKey);
     }
 
 }
--- addons/godot-near-sdk/utils/CryptoProxy.gd
+++ addons/godot-near-sdk/utils/CryptoProxy.gd
@@ -5,22 +5,14 @@ var crypto_helper_script = load("res://addons/godot-near-sdk/utils/CryptoHelper.
 func create_keypair() -> Dictionary:
 	var crypto_helper = crypto_helper_script.new()
 	crypto_helper.CreateKeyPair()
-	var public_key_bytes = crypto_helper.publicKey
-	var private_key_bytes = crypto_helper.privateKey
+	var public_key = crypto_helper.publicKey
+	var private_key = crypto_helper.privateKey
 	
-	# TODO: convert keys to NEAR-compatible strings through base58 encoding
-	
-	var output = ""
-	for b in public_key_bytes:
-		output += str(b) + ","
-	print("Public key GDScript: " + output)
-	output = ""
-	for b in private_key_bytes:
-		output += str(b) + ","
-	print("Private key GDScript: " + output)
+	print("Public key GDScript: " + public_key)
+	print("Private key GDScript: " + private_key)
 	
 	var keypair = {
-		"public_key": "",
-		"private_key": ""
+		"public_key": public_key,
+		"private_key": private_key
 	}
 	return keypair
